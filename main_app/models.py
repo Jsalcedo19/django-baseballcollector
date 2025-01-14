f53533f2ce1fd05480a748d5ef0bcc597fbd9bc4
Jose Salcedo <Jsalcedo19@gmail.com>
2025-01-14 11:53:50 -0500

    update database name and add Baseball and Games models with relationships

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,18 @@
 # Create your models here.
 from django.db import models
 
+# A tuple of 2-tuples
+Results = (
+    ('W', 'Win'),
+    ('L', 'Loss'),
+    ('N', 'Not played')
+)
+# new code above
+
+class Basseball(models.Model):
+    pass
+
+
 # Create your models here.
 class Baseball(models.Model):
     team_name = models.CharField(max_length=100)
@@ -10,3 +22,20 @@ class Baseball(models.Model):
 
     def __str__(self):
         return self.team_name
+
+# Add new Feeding model below Cat model
+class Games(models.Model):
+    date = models.DateField()
+    result = models.CharField(
+        max_length=1,
+        choices=Results,
+        # set the default value for results to be 'N' for Not played
+        default=Results[0][0]
+    )
+
+     # Create a baseball_id FK
+    Baseball = models.ForeignKey(Baseball, on_delete=models.CASCADE)
+
+    def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_game_display()} on {self.date}"
